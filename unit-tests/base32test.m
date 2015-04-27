c5dde397dc8e977c4ddfe343d0fdc4a6ac1b2f69
Robert Wapshott <robert.wapshott@forgerock.com>
2015-04-27 16:06:38 +0100

    Setup Unit Tests in XCode project.

---
+++ unit-tests/base32test.m
@@ -0,0 +1,40 @@
+//
+//  base32test.m
+//  ForgeRock
+//
+//  Created by Robert Wapshott on 27/04/2015.
+//  Copyright (c) 2015 ForgeRock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface base32test : XCTestCase
+
+@end
+
+@implementation base32test
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
